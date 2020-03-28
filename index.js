105b82787b03c3bca5db16bf8336ef6ef40b1eb5
brijkishor7828 <58461326+brijkishor7828@users.noreply.github.com>
2020-03-28 18:48:24 +0530

    gr

--- index.js
+++
@@ -1,9 +0,0 @@
-const core = require('@actions/core');
-const SMS = require('smsalert');
-
-const sms = new SMS(username, password);
-
-sms.send(to, message,senderid)
-    .then(body => console.log(body)) // returns { message_id: 'string' }
-  .catch(err => console.log(err.message))
-    core.debug('SMS sent!');
--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,29 @@
+"use strict";
 const core = require('@actions/core');
-const twilio = require('twilio');
-
+const SMS = require('smsalert');
 async function run() {
-  const from = core.getInput('fromPhoneNumber');
-  const to = core.getInput('toPhoneNumber');
-  const message = core.getInput('message');
-
-  const accountSid =
-    core.getInput('TWILIO_ACCOUNT_SID') || process.env.TWILIO_ACCOUNT_SID;
-  const apiKey = core.getInput('TWILIO_API_KEY') || process.env.TWILIO_API_KEY;
-  const apiSecret =
-    core.getInput('TWILIO_API_SECRET') || process.env.TWILIO_API_SECRET;
-
-  core.debug('Sending SMS');
-  const client = twilio(apiKey, apiSecret, { accountSid });
-  const resultMessage = await client.messages.create({
-    from,
-    to,
-    body: message,
-  });
-  core.debug('SMS sent!');
-
-  core.setOutput('messageSid', resultMessage.sid);
-
-  return resultMessage;
+    const senderid = core.getInput('Senderid');
+    const to = core.getInput('toPhoneNumber');
+    const message = core.getInput('message');
+    const username = core.getInput('SMSALERT_USERNAME') || process.env.SMSALERT_USERNAME;
+    const password = core.getInput('SMSALERT_PASSWORD') || process.env.SMSALERT_PASSWORD;
+   
+    core.debug('Sending SMS');
+	const sms = new SMS(username, password)
+	sms.send(to, message,senderid)
+    .then(body => console.log(body)) // returns { message_id: 'string' }
+  .catch(err => console.log(err.message))
+    core.debug('SMS sent!');
+    return body;
 }
-
 async function execute() {
-  try {
-    return await run();
-  } catch({ message }) {
-    core.error('Failed to send message', message);
-    core.setFailed(message);
-  }
+    try {
+        return await run();
+    }
+    catch ({ message }) {
+        core.error('Failed to send message', message);
+        core.setFailed(message);
+    }
 }
-
 module.exports = execute;
-
 execute();
